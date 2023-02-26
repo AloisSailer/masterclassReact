872fab2af6f2bec254fc48387fbe17e41a361a9a
Alois Sailer <alois_sailer@yahoo.com.br>
2023-02-25 22:51:18 -0300

    tweets

--- ui-twitter/src/main.tsx
+++ ui-twitter/src/main.tsx
@@ -2,40 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./components/global.css"
 
-import { Tweet } from "./components/Tweet"
 import { Sidebar } from "./components/Sidebar"
-import { Header } from "./components/Header"
-import { Separator } from "./components/Separator"
-
-const tweets = [
-  "Meu primeiro tweet",
-  "Teste",
-  "Deu certo tweetar!",
-  "Começando um novo assunto",
-]
+import { RouterProvider } from "react-router-dom"
+import { router } from "./components/routes"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
       <Sidebar />
       <div className="content">
-        <main className="timeLine">
-          <Header title="Home" />
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img
-                src="https://github.com/aloissailer.png"
-                alt="Foto Alois Sailer"
-              />
-              <textarea id="tweet" placeholder="What's happening?" />
-            </label>
-            <button type="submit">Tweet</button>
-          </form>
-          <Separator />
-          {tweets.map((tweet) => {
-            return <Tweet key={tweet} content={tweet} />
-          })}
-        </main>
+        <RouterProvider router={router} />
       </div>
     </div>
   </React.StrictMode>
